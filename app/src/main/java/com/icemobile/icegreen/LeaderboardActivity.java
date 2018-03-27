c0d11a861f19e6eca87318080a09b41fb4da70f0
Andy <andrew.cameron@icemobile.com>
2018-03-27 16:04:25 +0200

    Final alignment for working visuals

--- app/src/main/java/com/icemobile/icegreen/LeaderboardActivity.java
+++ app/src/main/java/com/icemobile/icegreen/LeaderboardActivity.java
@@ -12,46 +12,6 @@ import java.util.List;
 
 public class LeaderboardActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    ProfileAdapter adapter;
-
-    List<Profile> profileList;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_leaderboard);
-
-        profileList = new ArrayList<>();
-
-        recyclerView = (RecyclerView) findViewById(R.id.leaderboard_recycler);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        profileList.add(
-                new Profile(
-                        "Test Name 1",
-                        2,
-                        32));
-        profileList.add(
-                new Profile(
-                        "Test Name 3",
-                        2,
-                        12));
-        profileList.add(
-                new Profile(
-                        "Test Name 22",
-                        2,
-                        8));
-        profileList.add(
-                new Profile(
-                        "Test Name 66",
-                        2,
-                        58));
-
-        adapter = new ProfileAdapter(this, profileList);
-        recyclerView.setAdapter(adapter);
-    }
     public void returnToProfile(View view) {
         Intent myIntent = new Intent(LeaderboardActivity.this, ProfileActivity.class);
         startActivity(myIntent);
