86cd218b13826daebd32e4ec5e6367adbe8c2a5b
Andy <andrew.cameron@icemobile.com>
2018-04-03 15:21:41 +0200

    General changes made

--- app/src/main/java/com/icemobile/icegreen/ProfileActivity.java
+++ app/src/main/java/com/icemobile/icegreen/ProfileActivity.java
@@ -1,16 +1,21 @@
 package com.icemobile.icegreen;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+    //AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
     }
 
     public void returnToLogin(View view) {
